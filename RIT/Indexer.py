7026b0cf57968dc5f476882c166f7dfad498bec6
Unknown <andreymendozaf@gmail.com>
2017-08-19 15:44:13 -0600

    Stopwrods leidas

--- RIT/Indexer.py
+++ RIT/Indexer.py
@@ -1,9 +1,31 @@
+import re
+import os
+
 class Indexer:
 
-    def __init__(self):
+    def __init__(self):#, stopwordsDir, collectionDir, prefix):
 
+        self.stopwordsDir = '..\stopwords.txt'
         self.stopwords = []
+        self.collection = '..\man.es'
+        self.prefix = 'tests'
+
+    def readStopwords(self):
+
+        '''
+        Lee un archivo y toma cada termino por linea como un stopword y lo guarda en el atributo 'stopwords'.
+        :return:
+        '''
+
+        file = open(self.stopwordsDir, 'r')
+        for line in file:
+            word = re.findall(r'\w\w*', line)            #Ignora los espacios para tomar solo el termino
+            self.stopwords += word
+        file.close()
 
 
-    def readStopwords(self, dir):
 
+
+a = Indexer()
+a.readStopwords()
+print(a.stopwords)
